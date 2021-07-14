649b448e917c21187e119a1e70f6c4534e37d894
aladinor <aladinor@unal.edu.co>
2021-07-14 08:46:45 -0500

    first version of multi panel plot

--- src/sand_box.py
+++ src/sand_box.py
@@ -1,28 +1,57 @@
-import h5py
-import matplotlib.pyplot as plt
 import glob
-from pyart.graph import cm
-import datetime
+import drpy
+import matplotlib.pyplot as plt
+import numpy as np
+import cartopy.crs as ccrs
+import shapely.geometry as sgeom
+from apr3_read import hdf2xr
+from matplotlib.gridspec import GridSpec
 
 
-def utils(file_obj):
-    time = datetime.datetime(*[int(i) for i in file_obj['params_KUKA']['date_beg'][:]])
-    return time
+def plot_multi_panel(lores):
+    for i in range(lores.zhh14.shape[2]):
+        fig = plt.figure(figsize=(10, 8))
+        gs = GridSpec(2, 2, figure=fig)
+        ax1 = fig.add_subplot(gs[0, 0], projection=ccrs.PlateCarree())
+        ax2 = fig.add_subplot(gs[0, 1])
+        ax3 = fig.add_subplot(gs[-1, :])
+
+        lon = lores.lon[12, :].values.copy()
+        lat = lores.lat[12, :].values.copy()
+        ax1.set_extent([lon.max() + 2, lon.min() - 2, lat.min() - 2, lat.max() + 2], crs=ccrs.Geodetic())
+        lon[i + 1:] = np.nan
+        lat[i + 1:] = np.nan
+        track = sgeom.LineString(zip(lon, lat))
+        ax1.add_geometries([track], ccrs.PlateCarree(), facecolor='none', edgecolor='blue',
+                           linewidth=2)
+        ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=2, color='gray',
+                      alpha=0.5, linestyle='--')
+        ax1.coastlines()
+
+        x = lores.range[:] * np.sin(np.deg2rad(lores.azimuth[:]))
+        y = lores.range[:] * np.cos(np.deg2rad(lores.azimuth[:]))
+        a = ax2.pcolormesh(x[:, :, 0], y[:, :, 0], lores.zhh14[:, :, i],
+                           cmap=drpy.graph.cmaps.HomeyerRainbow, vmin=-10, vmax=40)
+        ax2.invert_yaxis()
+        plt.colorbar(a, ax=ax2, orientation='vertical')
+
+        dbz = lores.zhh14.values.copy()
+        dbz[:, 12, i:] = np.nan
+        c = ax3.pcolormesh(lores.time3d[:, 12, :], lores.alt3D[:, 12, :], dbz[:, 12, :],
+                           cmap=drpy.graph.cmaps.HomeyerRainbow, vmin=0, vmax=40)
+        plt.colorbar(c, ax=ax3)
+        plt.savefig(f'/home/alfonso/Documents/camp2ex_proj/results/Ka/ref/dual_plot_{i}')
 
 
 def main():
     path_file = '../data'
     files = glob.glob(f'{path_file}/*Wn.h5')
     files.sort()
-    hdf_ds = h5py.File(files[-2], 'r')
-    lores = hdf_ds['lores']['zhh35'][:]
-    for i in range(lores.shape[0]):
-        plt.close('all')
-        fig, ax = plt.subplots(figsize=(15, 4))
-        a = ax.imshow(lores[i, :, :], cmap='pyart_NWSRef', vmin=-10, vmax=35)
-        ax.set_aspect(2)
-        fig.colorbar(a, ax=ax, orientation='horizontal')
-        plt.savefig(f'../results/Ka/ref/lores_ref_ka_{i}')
+    for file in files[-2:-1]:
+        ds_dict = hdf2xr(file, groups=['lores'])
+        lores = ds_dict['lores']
+        plot_multi_panel(lores)
+
 
 
 if __name__ == '__main__':
