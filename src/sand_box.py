885d976aedea61254a2c5b6aa34b063aaacd7c9a
aladinor <aladinor@unal.edu.co>
2021-07-19 09:33:49 -0500

    fixing relative coordinates in ax2 plot

--- src/sand_box.py
+++ src/sand_box.py
@@ -9,7 +9,7 @@ from matplotlib.gridspec import GridSpec
 
 
 def plot_multi_panel(lores):
-    for i in range(lores.zhh14.shape[2]):
+    for i in range(lores.zhh35.shape[2]):
         fig = plt.figure(figsize=(10, 8))
         gs = GridSpec(2, 2, figure=fig)
         ax1 = fig.add_subplot(gs[0, 0], projection=ccrs.PlateCarree())
@@ -27,20 +27,20 @@ def plot_multi_panel(lores):
         ax1.gridlines(crs=ccrs.PlateCarree(), draw_labels=True, linewidth=2, color='gray',
                       alpha=0.5, linestyle='--')
         ax1.coastlines()
-
+        dbz = lores.zhh35.values.copy()
         x = lores.range[:] * np.sin(np.deg2rad(lores.azimuth[:]))
-        y = lores.range[:] * np.cos(np.deg2rad(lores.azimuth[:]))
-        a = ax2.pcolormesh(x[:, :, 0], y[:, :, 0], lores.zhh14[:, :, i],
+        # y = lores.range[:] * np.cos(np.deg2rad(lores.azimuth[:]))  # for plane relative coordinates
+        y = lores.alt3D.values * np.cos(np.deg2rad(lores.azimuth.values))
+        a = ax2.pcolormesh(x[:, :, i], y[:, :, i], dbz[:, :, i],
                            cmap=drpy.graph.cmaps.HomeyerRainbow, vmin=-10, vmax=40)
-        ax2.invert_yaxis()
+        # ax2.invert_yaxis()  # for plane relative coordinates
         plt.colorbar(a, ax=ax2, orientation='vertical')
-
-        dbz = lores.zhh14.values.copy()
         dbz[:, 12, i:] = np.nan
         c = ax3.pcolormesh(lores.time3d[:, 12, :], lores.alt3D[:, 12, :], dbz[:, 12, :],
                            cmap=drpy.graph.cmaps.HomeyerRainbow, vmin=0, vmax=40)
         plt.colorbar(c, ax=ax3)
-        plt.savefig(f'/home/alfonso/Documents/camp2ex_proj/results/Ka/ref/dual_plot_{i}')
+        # plt.show()
+        plt.savefig(f'../results/Ka/ref/dual_plot_2_{i:03}')
 
 
 def main():
@@ -51,7 +51,7 @@ def main():
         ds_dict = hdf2xr(file, groups=['lores'])
         lores = ds_dict['lores']
         plot_multi_panel(lores)
-
+        print('pause')
 
 
 if __name__ == '__main__':
