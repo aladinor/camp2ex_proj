40b2747c3997b3980e20395710bb3952ffb3e333
aladinor <aladinor@unal.edu.co>
2021-07-27 17:35:50 -0500

    getting rid of time vector

--- src/apr3_read.py
+++ src/apr3_read.py
@@ -5,23 +5,9 @@ import h5py
 import xarray as xr
 import numpy as np
 from utils import get_pars_from_ini
-from datetime import datetime
 
 
-def get_time(time_array, numbers):
-    """
-    Functions that creates a 3d time array from timestamps
-    :param time_array: 2d timestamp array
-    :param numbers: number of times in the new axis
-    :return: 3d time array
-    """
-    v_func = np.vectorize(lambda x: datetime.fromtimestamp(x))
-    _time = v_func(time_array)
-    time_3d = np.repeat(_time[np.newaxis, :, :], numbers, axis=0)
-    return time_3d
-
-
-def hdf2xr(h5_path, groups=None):
+def hdf2xr(h5_path, groups=None, campaign='Camp2ex'):
     """
     Function that converts CAMP2EX files (hdf5 files) to xarray datasets
     :param groups: list of Dataset to retrieve e.g. ['lores', 'hires']. If None, all groups will be retrieved.
@@ -38,10 +24,10 @@ def hdf2xr(h5_path, groups=None):
         ds = xr.Dataset()
         for key in members[group]:
             if h5f[group][key].size == 1:
-                attr_dict = {'data': h5f[group][key][:][0],
-                             'units': dt_params[group][key]['units'],
+                attr_dict = {'units': dt_params[group][key]['units'],
                              'notes': dt_params[group][key]['notes']}
-                ds.attrs[key] = attr_dict
+                ds[key] = xr.DataArray(h5f[group][key][:][0],
+                                       attrs=attr_dict)
             elif h5f[group][key].ndim == 2:
                 attr_dict = {'units': dt_params[group][key]['units'],
                              'notes': dt_params[group][key]['notes']}
@@ -58,7 +44,6 @@ def hdf2xr(h5_path, groups=None):
                 try:
                     attr_dict = {'units': dt_params[group][key]['units'],
                                  'notes': dt_params[group][key]['notes']}
-                    time3d = get_time(h5f[group]['scantime'][:], numbers=h5f[group][key].shape[0])
                     da = xr.DataArray(h5f[group][key][:],
                                       dims={'range': np.arange(h5f[group][key].shape[0]),
                                             'cross_track': np.arange(h5f[group][key].shape[1]),
@@ -66,7 +51,6 @@ def hdf2xr(h5_path, groups=None):
                                       coords={
                                           'lon3d': (['range', 'cross_track', 'along_track'], h5f[group]['lon3D'][:]),
                                           'lat3d': (['range', 'cross_track', 'along_track'], h5f[group]['lat3D'][:]),
-                                          'time3d': (['range', 'cross_track', 'along_track'], time3d),
                                           'alt3d': (['range', 'cross_track', 'along_track'], h5f[group]['alt3D'][:])},
                                       attrs=attr_dict)
                     ds[key] = da
@@ -85,11 +69,16 @@ def hdf2xr(h5_path, groups=None):
 
 
 def main():
+    import time
+    now = time.time()
     path_file = '../data'
     files = glob.glob(f'{path_file}/*Wn.h5')
     files.sort()
-    ds = hdf2xr(files[-2])
-    print(ds)
+    for i, file in enumerate(files):
+        ds = hdf2xr(file, groups=['lores'])
+        ds['lores'].to_zarr(store=f'/home/alfonso/Documents/camp2ex_proj/zarr/apr3.zarr', mode='a',
+                            append_dim='along_track')
+    print(time.time() - now)
 
 
 if __name__ == '__main__':
