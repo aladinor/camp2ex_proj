52fa9853c79e858fef3818655a04a552e1f80654
aladinor <aladinor@unal.edu.co>
2021-08-18 19:42:32 -0500

    fixing bugs with files compabilities

--- src/zarr_test.py
+++ src/zarr_test.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 import glob
 import time
-from apr3_read import hdf2xr
 import xarray as xr
+from apr3_read import hdf2xr
+from utils import make_dir
 
 
 def data_test(path_file):
@@ -13,29 +14,38 @@ def data_test(path_file):
 
 def hdf2zar(path_file):
     now = time.time()
-    files = glob.glob(f'{path_file}/data/*.h5')
-    files = [item for item in files if '_c1.h5' not in item]
-    files.sort()
-    with open(f'{path_file}/good_files.txt', 'w') as good,  \
-            open(f'{path_file}/bad_files.txt', 'w') as bad:
-        for i, file in enumerate(files):
-            print(i, file)
-            ds = hdf2xr(file)
-            args = {'consolidated': True}
-            if i == 0:
-                args['mode'] = 'w'
-            else:
-                args['mode'] = 'a'
-                args['append_dim'] = 'time'
-            try:
-                for key in ds:
-                    ds[key].to_zarr(store=f'{path_file}/zarr/{key}.zarr', **args)
-                    good.writelines(f"{file.split('/')[-1]}\n")
-            except ValueError as e:
-                bad.writelines(f"{file.split('/')[-1]}, {e}\n")
-        del ds
-    good.close()
-    bad.close()
+    files_dict = {}
+    files_dict['Wn'] = glob.glob(f'{path_file}/data/*_Wn.h5')
+    files_dict['KUsKAsWs'] = glob.glob(f'{path_file}/data/*_KUsKAsWs.h5')
+    files_dict['KUsKAs_Wn'] = glob.glob(f'{path_file}/data/*_KUsKAs.h5') + glob.glob(f'{path_file}/data/*_KUsKAsWn.h5')
+
+    for key_outer in files_dict:
+        files = files_dict[key_outer]
+        files.sort()
+        make_dir(f'{path_file}/zarr/{key_outer}')
+        with open(f'{path_file}/zarr/{key_outer}/good_files.txt', 'w') as good,  \
+                open(f'{path_file}/zarr/{key_outer}/bad_files.txt', 'w') as bad:
+            for i, file in enumerate(files):
+                print(i, file)
+                ds = hdf2xr(file)
+                args = {'consolidated': True}
+                for key in ds.keys():
+                    if i == 0:
+                        args['mode'] = 'w'
+                    else:
+                        args['mode'] = 'a'
+                        if not hasattr(ds[key], 'time'):
+                            args['append_dim'] = 'params'
+                        else:
+                            args['append_dim'] = 'time'
+                    try:
+                        ds[key].to_zarr(store=f'{path_file}/zarr/{key_outer}/{key}.zarr', **args)
+                        good.writelines(f"{file.split('/')[-1]}, {ds.keys()}, {key}, \n")
+                    except (ValueError, KeyError, IndexError) as e:
+                        bad.writelines(f"{file.split('/')[-1]}, {ds.keys()}, {key}, {e}\n")
+                del ds
+        good.close()
+        bad.close()
     print(time.time() - now)
 
 
