b5f7ab95d1e982b184ea1e2869fface3bf7df0eb
aladinor <aladinor@unal.edu.co>
2021-09-06 12:21:31 -0500

    deleting file and moving hfd2zarr to apr.py file

--- src/zarr_filter.py
+++ src/zarr_filter.py
@@ -2,6 +2,20 @@
 # -*- coding: utf-8 -*-
 import xarray as xr
 import numpy as np
+from scipy.ndimage.filters import uniform_filter
+from scipy.ndimage.measurements import variance
+
+
+def lee_filter(img, size):
+    img_mean = uniform_filter(img, (size, size))
+    img_sqr_mean = uniform_filter(img**2, (size, size))
+    img_variance = img_sqr_mean - img_mean**2
+
+    overall_variance = variance(img)
+
+    img_weights = img_variance / (img_variance + overall_variance)
+    img_output = img_mean + img_weights * (img - img_mean)
+    return img_output
 
 
 def rolling_window(a, window):
@@ -25,7 +39,8 @@ def texture(fld):
 
 
 def data_test(path_file):
-    ds_xr = xr.open_zarr(f'{path_file}/zarr/KUsKAs_Wn/lores.zarr', consolidated=True, decode_times=True)
+    ds_xr = xr.open_zarr(f'{path_file}/zarr/KUsKAs_Wn/lores.zarr')
+
     _events = []
     with open(f'{path_file}/zarr/events.txt', 'w') as events:
         for date in ds_xr.time:
