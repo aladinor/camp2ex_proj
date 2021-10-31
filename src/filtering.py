70cbdccc1ec02b3a863c689cb1a94f032d2a6ff7
aladinor <aladinor@unal.edu.co>
2021-10-31 09:48:26 -0500

    making regrid function works with dask

--- src/filtering.py
+++ src/filtering.py
@@ -9,10 +9,11 @@ import dask.array as da
 import numpy as np
 from re import split
 from scipy.spatial import cKDTree as KDTree
-
+from scipy.interpolate import griddata
 from skimage.filters import gaussian, threshold_otsu
 from skimage import measure
-from scipy.interpolate import griddata
+from dask import delayed
+import dask
 from dask_image.ndfilters import uniform_filter as uf
 from dask_image.ndmeasure import variance as varian
 import matplotlib.patches as mpatches
@@ -43,8 +44,6 @@ def excluding_mesh(x, y, nx=30, ny=30):
     xp = xp.ravel()
     yp = yp.ravel()
 
-    # Use KDTree to answer the question: "which point of set (x,y) is the
-    # nearest neighbors of those in (xp, yp)"
     tree = KDTree(np.c_[x, y])
     dist, j = tree.query(np.c_[xp, yp], k=1)
 
@@ -59,29 +58,31 @@ def regridd(data, x, y, size=30):
     size = desired pixel size in meters
     """
     if data.ndim > 2:
-        x_s = np.asarray([x[:, :, i].flatten() for i in range(x.shape[-1])])
-        y_s = np.asarray([y[:, :, i].flatten() for i in range(y.shape[-1])])
+        x_n = np.rollaxis(x.reshape(-1, x.shape[-1]), 1)
+        y_n = np.rollaxis(y.reshape(-1, y.shape[-1]), 1)
         z_s = data.compute()
-        z_s = np.asarray([z_s[:, :, i].flatten() for i in range(z_s.shape[-1])])
-        idx = np.asarray([x_s[i].argsort() for i in range(x_s.shape[0])])
-        x_s = np.asarray([np.take_along_axis(x_s[i], idx[i], axis=0) for i in range(x_s.shape[0])])
-        y_s = np.asarray([np.take_along_axis(y_s[i], idx[i], axis=0) for i in range(y_s.shape[0])])
-        z_s = np.asarray([np.take_along_axis(z_s[i], idx[i], axis=0) for i in range(z_s.shape[0])])
-        ncols = max(np.apply_along_axis(get_col_row, arr=x_s, axis=1))
-        nrows = max(np.apply_along_axis(get_col_row, arr=y_s, axis=1))
-        vp = np.asarray([excluding_mesh(x_s[i], y_s[i]) for i in range(x_s.shape[0])], dtype=object)
-        xp, yp = vp[:, 0], vp[:, 1]
-        zp = np.asarray([np.nan + np.zeros_like(xp[i]) for i in range(xp.shape[0])], dtype=object)
-        x_new = np.asarray([np.linspace(x_s[i, :].min(), x_s[i, :].max(), ncols) for i in range(xp.shape[0])])
-        y_new = np.asarray([np.linspace(y_s[i, :].max(), y_s[i, :].min(), nrows)
-                            for i in range(xp.shape[0])])
-        mesh = [np.meshgrid(x_new[i], y_new[i]) for i in range(x_new.shape[0])]
-        xi = np.asarray(mesh)[:, 0]
-        yi = np.asarray(mesh)[:, 1]
-        z0 = [griddata((np.r_[x_s[i, :], xp[i]], np.r_[y_s[i, :], yp[i]]), np.r_[z_s[i, :], zp[i]],
-                       (xi[i], yi[i]), method='linear', fill_value=-9999)
+        z_n = np.rollaxis(z_s.reshape(-1, z_s.shape[-1]), 1)
+        idx_n = x_n.argsort(axis=-1)
+        x_n = np.take_along_axis(x_n, idx_n, axis=-1)
+        y_n = np.take_along_axis(y_n, idx_n, axis=-1)
+        z_n = np.take_along_axis(z_n, idx_n, axis=-1)
+        ncols_n = max(np.apply_along_axis(get_col_row, arr=x_n, axis=1))
+        nrows_n = max(np.apply_along_axis(get_col_row, arr=y_n, axis=1))
+        vp_n = [delayed(excluding_mesh)(x_n[i], y_n[i]) for i in range(x_n.shape[0])]
+        vp_n = da.rollaxis(da.dstack(dask.compute(*vp_n)), -1)
+        xp_n, yp_n = vp_n[:, 0], vp_n[:, 1]
+        zp_n = [delayed(da.zeros_like)(xp_n[i]) for i in range(xp_n.shape[0])]
+        zp_n = da.rollaxis(da.dstack(dask.compute(*zp_n))[0], -1)
+        x_new_n = da.from_array(np.rollaxis(np.linspace(np.amin(x_n, -1), np.amax(x_n, -1), ncols_n), 1))
+        y_new_n = da.from_array(np.rollaxis(np.linspace(np.amax(y_n, -1), np.amin(y_n, -1), nrows_n), 1))
+        mesh = [delayed(da.meshgrid)(x_new_n[i], y_new_n[i]) for i in range(x_new_n.shape[0])]
+        mesh = dask.compute(*mesh)
+        xi = da.asarray(mesh)[:, 0]
+        yi = da.asarray(mesh)[:, 1]
+        z0 = [delayed(griddata)((np.r_[x_n[i, :], xp_n[i]], np.r_[y_n[i, :], yp_n[i]]), np.r_[z_n[i, :], zp_n[i]],
+                                (xi[i], yi[i]), method='linear', fill_value=-9999)
               for i in range(xi.shape[0])]
-        z0 = np.dstack(z0)
+        z0 = da.dstack(dask.compute(*z0))
         return z0
     else:
         x_s = x.flatten()
@@ -122,7 +123,6 @@ def process_new(zhh14, _range, azimuth, alt3d, bin_size, time):
     y = alt3d * np.cos(np.deg2rad(azimuth.T))
     img_filtered = lee_filter_new(zhh14, size=3, tresh=-200)
     img = regridd(img_filtered, x.values, y.values)
-    # img = lee_filter_new(img, size=5, tresh=-300)
     img = np.where(img > 0., img, 0.)
     blurred = gaussian(img, sigma=0.8)
     binary = blurred > threshold_otsu(blurred)
@@ -139,35 +139,29 @@ def process_new(zhh14, _range, azimuth, alt3d, bin_size, time):
                        [prop.bbox]] for prop in props]
         df = pd.DataFrame(data=_props_all, columns=['area', 'perimeter', 'axmax', 'axmin', 'bbox'])
 
-
     if img.ndim > 2:
         shp = img.shape[-1]
         for i in range(shp):
             fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 10))
-            # img_plot = np.where(img[i] > 0, img[i], np.nan)
             img_plot = img[:, :, i]
             ax1.pcolormesh(x.isel(time=i), y.isel(time=i), img_filtered.compute()[:, :, i], cmap='jet',
                            vmax=40, vmin=0, shading='auto')
             ax2.imshow(img_plot, aspect='auto', cmap='jet', vmax=40, vmin=0)
             for region in props[i]:
-                # take regions with large enough areas
                 if region.area >= 100:
-                    # draw rectangle around segmented coins
                     minr, minc, maxr, maxc = region.bbox
                     rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                               fill=False, edgecolor='red', linewidth=2)
                     ax2.add_patch(rect)
             plt.show()
+
     else:
         fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 10))
-        # img_plot = np.where(img[i] > 0, img[i], np.nan)
         img_plot = img
         ax1.pcolormesh(x, y, img_filtered.compute(), cmap='jet', vmax=40, vmin=0)
         ax2.imshow(img_plot, aspect='auto', cmap='jet', vmax=40, vmin=0)
         for region in props:
-            # take regions with large enough areas
             if region.area >= 100:
-                # draw rectangle around segmented coins
                 minr, minc, maxr, maxc = region.bbox
                 rect = mpatches.Rectangle((minc, minr), maxc - minc, maxr - minr,
                                           fill=False, edgecolor='red', linewidth=2)
@@ -179,7 +173,7 @@ def process_new(zhh14, _range, azimuth, alt3d, bin_size, time):
 
 
 def main():
-    ds_xr = xr.open_zarr('C:/Users/alfonso8/Documents/python/camp2ex_proj/zarr/lores.zarr', consolidated=True)
+    ds_xr = xr.open_zarr(f'{path_data}/zarr/KUsKAs_Wn/lores.zarr', consolidated=True)
     ds_xr = ds_xr.sel(time=~ds_xr.get_index("time").duplicated())
     # ds_data = ds_xr[['zhh14', 'azimuth', 'DR']].sel(time='2019-09-16 03:12:58').isel(time=0)
     ds_data = ds_xr[['zhh14', 'azimuth', 'DR']].sel(time=slice('2019-09-16 03:12:50', '2019-09-16 03:13:05'))
