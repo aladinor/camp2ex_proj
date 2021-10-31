644704dc94d4147e7e8f0574e207dd4f4bc67628
aladinor <aladinor@unal.edu.co>
2021-10-31 14:00:15 -0500

    returning a xarray dataset from xarray ufucnt

--- src/ufucnt_xr.py
+++ src/ufucnt_xr.py
@@ -42,10 +42,9 @@ def excluding_mesh(x, y, nx=30, ny=30):
     dy = y.ptp() / ny
 
     xp, yp = np.mgrid[x.min() - 2 * dx:x.max() + 2 * dx:(nx + 2) * 1j,
-                      y.min() - 2 * dy:y.max() + 2 * dy:(ny + 2) * 1j]
+             y.min() - 2 * dy:y.max() + 2 * dy:(ny + 2) * 1j]
     xp = xp.ravel()
     yp = yp.ravel()
-
 
     tree = KDTree(np.c_[x, y])
     dist, j = tree.query(np.c_[xp, yp], k=1)
@@ -86,7 +85,7 @@ def regridd(data, x, y, size=30):
                                 (xi[i], yi[i]), method='linear', fill_value=-9999)
               for i in range(xi.shape[0])]
         z0 = da.dstack(dask.compute(*z0))
-        return z0
+        return z0, da.rollaxis(da.rollaxis(xi, axis=-1), axis=-1), da.rollaxis(da.rollaxis(yi, axis=-1), axis=-1)
     else:
         x_s = x.flatten()
         y_s = y.flatten()
@@ -102,7 +101,7 @@ def regridd(data, x, y, size=30):
         xp, yp = excluding_mesh(x_s, y_s, nx=35, ny=35)
         zp = np.nan + np.zeros_like(xp)
         z0 = griddata((np.r_[x_s, xp], np.r_[y_s, yp]), np.r_[z_s, zp], (xi, yi), method='linear', fill_value=-9999)
-        return z0
+        return z0, xi, yi
 
 
 def lee_filter_new(img, size, tresh=-150):
@@ -124,7 +123,15 @@ def lee_filter_new(img, size, tresh=-150):
 def process_new(zhh14, x, y, time):
     x = x[:, 0, :, :]
     img_filtered = lee_filter_new(zhh14, size=3, tresh=-200)
-    img = regridd(img_filtered, x, y)
+    img, xi, yi = regridd(img_filtered, x, y)
+    if zhh14.ndim > 2:
+        total, _x, _y = regridd(img_filtered[:, :, 0], x[:, :, 0], y[:, :, 0])
+        total = np.nansum(np.where(total >= 0, 1, 0), axis=1)
+        total = np.repeat(total[:, np.newaxis], img_filtered.shape[-1], 1)
+    else:
+        total = np.nansum(np.where(img >= 0, 1, 0), axis=1)  # Total of number of pixels
+    num_pixels = np.nansum(np.where(img > 0, 1, np.nan), axis=1)
+    weigt = num_pixels / total
     img = np.where(img > 0., img, 0.)
     blurred = gaussian(img, sigma=0.8)
     binary = blurred > threshold_otsu(blurred)
@@ -139,46 +146,50 @@ def process_new(zhh14, x, y, time):
         props = measure.regionprops(labels)
         _props_all = [[[prop.area], [prop.perimeter], [prop.major_axis_length], [prop.minor_axis_length],
                        [prop.bbox]] for prop in props]
-        df = pd.DataFrame(data=_props_all, columns=['area', 'perimeter', 'axmax', 'axmin', 'bbox'])
+        df = pd.DataFrame(data=_props_all, columns=['area', 'perimeter', 'axmax', 'axmin', 'bbox'], index=time)
 
-    df = df.explode(['area', 'perimeter', 'axmax', 'axmin'])
-    df.to_csv(f'../results/all_{len(time)}.csv')
-    df = df.astype(dtype={'area': 'float', 'perimeter': 'float', 'axmax': 'float', 'axmin': 'float'})
-    df = df[df.area > 50.0]
-    df_new = pd.DataFrame(index=time, data=np.full(len(time), np.nan), columns=['area'])
-    df_new = df_new.merge(df, left_index=True, right_index=True, how='left').drop(['area_x'], axis=1)
-    idx = df_new.index.duplicated()
-    xr_prop = xr.Dataset.from_dataframe(df_new[~idx]).rename_dims({'index': 'time'}).rename({'index': 'time'})
-    return xr_prop.area_y, xr_prop.perimeter, xr_prop.axmax, xr_prop.axmin
+    # df = df.explode(['area', 'perimeter', 'axmax', 'axmin'])
+    # df.to_csv(f'../results/all_{len(time)}.csv')
+    # df = df.astype(dtype={'area': 'float', 'perimeter': 'float', 'axmax': 'float', 'axmin': 'float'})
+    # df = df[df.area > 50.0]
+    # df_new = pd.DataFrame(index=time, data=np.full(len(time), np.nan), columns=['area'])
+    # df_new = df_new.merge(df, left_index=True, right_index=True, how='left').drop(['area_x'], axis=1)
+    # idx = df_new.index.duplicated()
+    xr_prop = xr.Dataset.from_dataframe(df).rename_dims({'index': 'time'}).rename({'index': 'time'})
+    # xr_prop['x'] = xr.DataArray(xi, dims=['cross_track', 'height', 'time'],
+    #                             coords={'x': (['cross_track', 'height', 'time'], x)})
+    # xr_prop['y'] = xr.DataArray(yi, dims={'time': time})
+    return xr_prop.area, xr_prop.perimeter, xr_prop.axmax, xr_prop.axmin, xr_prop.bbox
 
 
 def ufunc_wrapper(data):
-    x = data.range * data.DR * np.sin(np.deg2rad(data.azimuth)) # add roll
+    x = data.range * data.DR * np.sin(np.deg2rad(data.azimuth))  # add roll
     y = data.alt3d * np.cos(np.deg2rad(data.azimuth))
     zhh = data.zhh14.where(data.alt3d > 500)
     _data = [zhh, x, y, data.time]
     icd = [list(i.dims) for i in _data]
     dfk = {'allow_rechunk': True, 'output_sizes': {}}
-    a, p, mx, mn = xr.apply_ufunc(process_new,
-                                  *_data,
-                                  input_core_dims=icd,
-                                  output_core_dims=[["time"], ["time"], ["time"], ["time"]],
-                                  dask_gufunc_kwargs=dfk,
-                                  dask='parallelized',
-                                  vectorize=True,
-                                  output_dtypes=[(float), (float), (float), (float)]
-                                  )
+    a, p, mx, mn, bbox = xr.apply_ufunc(process_new,
+                                        *_data,
+                                        input_core_dims=icd,
+                                        output_core_dims=[["time"], ["time"], ["time"], ["time"], ["time"]],
+                                        dask_gufunc_kwargs=dfk,
+                                        dask='parallelized',
+                                        vectorize=True,
+                                        output_dtypes=[(object), (object), (object), (object), (object)]
+                                        )
     ds_out = a.to_dataset(name='area')
     ds_out['perimeter'] = p
     ds_out['ax_max'] = mx
     ds_out['ax_min'] = mn
+    ds_out['bbox'] = bbox
     return ds_out
 
 
 def main():
     ds_xr = xr.open_zarr(f'{path_data}/zarr/KUsKAs_Wn/lores.zarr', consolidated=True)
     ds_xr = ds_xr.sel(time=~ds_xr.get_index("time").duplicated())
-    # ds_data = ds_xr[['zhh14', 'azimuth', 'DR']].sel(time='2019-09-16 03:12:58').isel(time=0)
+    # ds_data = ds_xr[['zhh14', 'azimuth', 'DR']].sel(time='2019-09-16 03:12:58')
     ds_data = ds_xr[['zhh14', 'azimuth', 'DR']].sel(time=slice('2019-09-16 03:12:50', '2019-09-16 03:13:05'))
     a = ufunc_wrapper(ds_data)
     w = dask.compute(a)
