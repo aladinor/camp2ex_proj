099c5c5c314635ef859bd51d445c3d8da94bbdf8
aladinor <aladinor@unal.edu.co>
2022-08-15 08:08:27 -0500

    adding additional data to the xarray dataset

--- src/bimodality_dask.py
+++ src/bimodality_dask.py
@@ -83,7 +83,9 @@ def get_data(instrument='Lear', temp=2):
             if temp:
                 df = df[df['Temp'] >= temp]
             p3_df[i] = df
-        return p3_df
+        p3_dd = [dd.from_pandas(i, npartitions=1) for i in p3_df]
+        del p3_df
+        return p3_dd
     else:
         raise TypeError(f"{instrument} not available. Use Lear or P3B")
 
@@ -129,49 +131,94 @@ def get_intervals(ls_df):
     return cols
 
 
-def linear_wgt(df1, df2, ovr_upp=1200, ovr_lower=800):
+def apply_wgt(df, ovr_upp, ovr_lower):
+    if (df['2DS10'] != 0) & (df['HVPS'] != 0):
+        df['2ds10_wgt'] = df['2DS10'] * df['2DS10'].index.values / (ovr_upp - ovr_lower)
+        df['hvps_wgt'] = df['HVPS'] * (df['HVPS'].index.values - ovr_lower) / (ovr_upp - ovr_lower)
+        df['nd_res'] = df[['2ds10_wgt', 'hvps_wgt']].dropna(how='all').sum(1)
+        return df['nd_res']
+    elif (df['2DS10'] == 0) & (df['HVPS'] != 0):
+        return df['HVPS']
+    elif (df['2DS10'] != 0) & (df['HVPS'] == 0):
+        return df['HVPS']
+    else:
+        return np.nan
+
+
+def linear_wgt(df1, df2, ovr_upp=1200, ovr_lower=800, method='linear'):
     """
 
+    :param method: method to apply. linear applies Leroy et al. 2014. swal Applies Snesbitt method
     :param df1: pandas series with the small diameter sizes (e.g. 2DS10)
     :param df2: pandas series with the small diameter sizes (e.g. HVPS)
     :param ovr_upp: upper limit for overlapping
     :param ovr_lower: lower limit for overlapping
     :return: pd series with a composite PSD
     """
-    cond1 = (df1.index > ovr_lower) & (df1.index < ovr_upp)
-    cond2 = (df2.index > ovr_lower) & (df2.index < ovr_upp)
+    df1.index = df1.attrs['2DS10']['intervals']
+    df2.index = df2.attrs['HVPS']['intervals']
+    cond1 = (df1.index.mid >= ovr_lower) & (df1.index.mid <= ovr_upp)
+    cond2 = (df2.index.mid >= ovr_lower) & (df2.index.mid <= ovr_upp)
     _nd_uppr = df1[cond1]
     _nd_lower = df2[cond2]
-    nd_overlap = pd.concat([_nd_lower, _nd_uppr],
-                           axis=1).reindex(np.arange(ovr_lower, ovr_upp, 5)).interpolate('pad').fillna(0).dropna(
-        how='all')
 
-    nd_overlap['nd_res'] = nd_overlap[nd_overlap.columns[0]] * (
-            ovr_upp - nd_overlap[nd_overlap.columns[0]].index.values) / \
-                           (ovr_upp - ovr_lower) + nd_overlap[nd_overlap.columns[-1]] * \
-                           (nd_overlap[nd_overlap.columns[-1]].index.values - ovr_lower) / (ovr_upp - ovr_lower)
+    nd_overlap = pd.concat([_nd_uppr.reindex(np.arange(ovr_lower, ovr_upp, 5)),
+                            _nd_lower.reindex(np.arange(ovr_lower, ovr_upp, 5))], axis=1)
+    if method == 'linear':
+        nd_overlap['2ds10_wgt'] = nd_overlap['2DS10'] * (ovr_upp - nd_overlap['2DS10'].index.values) / \
+                                  (ovr_upp - ovr_lower)
+        nd_overlap['hvps_wgt'] = nd_overlap['HVPS'] * (nd_overlap['HVPS'].index.values - ovr_lower) / \
+                                 (ovr_upp - ovr_lower)
+        nd_overlap['nd_res'] = nd_overlap[['2ds10_wgt', 'hvps_wgt']].dropna(how='all').sum(1)
+        nd_overlap = nd_overlap.reindex(df1[cond1].index.mid)
+        nd_overlap.index = df1[cond1].index
+        res = pd.concat([df1[df1.index.mid <= ovr_lower], nd_overlap['nd_res'], df2[df2.index.mid >= ovr_upp]])
+        res.index = res.index.mid
+        return res
 
-    nd_overlap = nd_overlap.reindex(df1[cond1].index)
-    # nd_overlap = nd_overlap.reindex(df2[cond2].index)
-    return pd.concat([df1[df1.index <= ovr_lower], nd_overlap['nd_res'], df2[df2.index >= ovr_upp]])
+    elif 'snal':
+        _sdd = nd_overlap.where(
+            (nd_overlap['2DS10'] != 0) & (nd_overlap['HVPS'] != 0) & (nd_overlap['2DS10'].notnull()) &
+            (nd_overlap['HVPS'] != 0).notnull()).dropna(how='any')
+        _sdd['2ds10_wgt'] = _sdd['2DS10'] * (ovr_upp - _sdd['2DS10'].index.values) / (ovr_upp - ovr_lower)
+        _sdd['hvps_wgt'] = _sdd['HVPS'] * (_sdd['HVPS'].index.values - ovr_lower) / (ovr_upp - ovr_lower)
+        _sdd['nd_res'] = _sdd[['2ds10_wgt', 'hvps_wgt']].sum(1)
+
+        _sdc = nd_overlap.where(((nd_overlap['2DS10'] == 0) | (nd_overlap['2DS10'].isnull())) &
+                                (nd_overlap['HVPS'] != 0)).dropna(how='all')['HVPS']
+        _sds = nd_overlap.where((nd_overlap['2DS10'] != 0) & ((nd_overlap['HVPS'] == 0) |
+                                                              (nd_overlap['HVPS'].isnull()))).dropna(how='all')['2DS10']
+        res = pd.concat([_sdd['nd_res'], _sdc, _sds]).sort_index()
+        res = res.reindex(df1[cond1].index.mid)
+        res.index = df1[cond1].index
+        res = pd.concat([df1[df1.index.mid <= ovr_lower], res, df2[df2.index.mid >= ovr_upp]])
+        res.index = res.index.mid
+        return res
 
 
 def vel(d):
     return -0.1021 + 4.932 * d - 0.955 * d ** 2 + 0.07934 * d ** 3 - 0.0023626 * d ** 4
 
 
-def pds_parameters(nd, d, dd):
+def pds_parameters(nd):
+    """
+    Compute the psd parameters
+    :param nd: partice size distribution in # L-1 um-1
+    :return: list with lwc, dm, nw, z, and r
+    """
     try:
-        lwc = (np.pi / 6) * 1e-3 * np.sum(nd * d ** 3 * dd)  # g / m3
+        d = np.fromiter(nd.index, dtype=float) / 1e3  # diameter in millimeters
+        dd = np.fromiter(nd.attrs['dsizes'].values(), dtype=float)  # d_size in um
+        lwc = (np.pi / 6) * np.sum(nd * d ** 3 * dd)  # g / m3
         dm = np.sum(nd * d ** 4 * dd) / np.sum(nd * d ** 3 * dd)  # mm
         nw = 1e3 * (4 ** 4 / np.pi) * (lwc / dm ** 4)
         z = np.sum(nd * d ** 6 * dd)
         r = np.pi * 6e-4 * np.sum(nd * d ** 3 * vel(d) * dd)
-        ref = ref_calc(nd=nd, d=d, dd=dd)
-        return pd.Series([lwc, dm, nw, z, r]).append(ref)
+        # ref = ref_calc(nd=nd, d=d, dd=dd)
+        # return pd.Series([lwc, dm, nw, z, r])
+        return [lwc, dm, nw, z, r]
     except ZeroDivisionError:
-        return pd.Series([np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan, np.nan])
-
+        return [np.nan, np.nan, np.nan, np.nan, np.nan]
 
 
 def bcksct(ds, instrument, ar=1, j=0) -> pd.DataFrame:
@@ -263,6 +310,49 @@ def get_attrs_merged(dt_attrs, _upper, _lower):
     return {**dt_2ds, **dt_hvps}
 
 
+def compute_hr(t, td):
+    """
+    Computes relative humidity using magnus approximation using
+    https://earthscience.stackexchange.com/questions/16570/how-to-calculate-relative-humidity-from-temperature-dew-point-and-pressure
+    :param t: temperature
+    :param td: dew point temperature
+    :return: relative humidity
+    """
+    b = 17.625
+    _c = 243.04
+    return 100 * np.exp((_c * b * (td - t) / ((_c + t) * (_c + td))))
+
+
+def get_add_data(aircraft: 'str', indexx) -> pd.DataFrame:
+    path_db = f'{path_data}/db'
+    str_db = f"sqlite:///{path_db}/camp2ex.sqlite"
+    if aircraft == 'Lear':
+        df_add = pd.read_sql_query("SELECT time, Temp, Dew, Palt, NevLWC, VaV  FROM Page0_Learjet", con=str_db)
+        df_add['time'] = df_add['time'].apply(pd.Timestamp).apply(lambda x: x.tz_localize('UTC'))
+        df_add['RH'] = df_add[['Temp', 'Dew']].apply(lambda x: compute_hr(t=x['Temp'], td=x['Dew']), axis=1)
+        df_add['RH'] = df_add['RH'].where(df_add['RH'] <= 100, 100)
+        df_add.index = df_add['time']
+        df_add.drop('time', axis=1, inplace=True)
+        cols = ['temp', 'dew_point', 'altitude(ft)', 'Nev_lwc', 'vertical_vel', 'RH']
+        new_cols = {j: cols[i] for i, j in enumerate(list(df_add.columns))}
+        df_add.rename(columns=new_cols, inplace=True)
+        df_add = df_add[(df_add.index >= indexx.min().strftime('%Y-%m-%d %X')) &
+                        (df_add.index <= indexx.max().strftime('%Y-%m-%d %X'))]
+        return df_add
+    else:
+        df_add = pd.read_sql_query("SELECT pbm.'time', pbm.' Total_Air_Temp_YANG_MetNav', pbm.' "
+                                   "Dew_Point_YANG_MetNav', pbm.' GPS_Altitude_YANG_MetNav', pbm.' LWC_gm3_LAWSON',  "
+                                   "pbm.' Vertical_Speed_YANG_MetNav', pbm.' Relative_Humidity_YANG_MetNav' FROM "
+                                   "p3b_merge pbm", con=str_db)
+        df_add['time'] = df_add['time'].apply(pd.Timestamp).apply(lambda x: x.tz_localize('UTC'))
+        df_add.index = df_add['time']
+        df_add.drop('time', axis=1, inplace=True)
+        cols = ['temp', 'dew_point', 'altitude(m)', 'Lawson_lwc', 'vertical_vel', 'RH']
+        new_cols = {j: cols[i] for i, j in enumerate(list(df_add.columns))}
+        df_add.rename(columns=new_cols, inplace=True)
+        return df_add
+
+
 def main():
     aircraft = 'Lear'
     _upper = 1200
@@ -272,6 +362,7 @@ def main():
     ls_df = filt_by_cols(ls_df)
     ls_df = compute(*ls_df)
     intervals = get_intervals(ls_df)
+
     for i, inter in enumerate(intervals):
         ls_df[i].attrs['intervals'] = inter
     instr = [i.attrs['instrument'] for i in ls_df]
@@ -280,7 +371,8 @@ def main():
     df_concat = pd.concat(compute(*ls_df), axis=1, keys=instr, levels=[instr])
     df_concat.attrs = dt_attrs
 
-    rdm_idx = pd.date_range(start='2019-09-07 2:31:50', periods=150, tz='UTC', freq='S')  # for Lear
+    rdm_idx = pd.date_range(start='2019-09-07 2:31:45', periods=150, tz='UTC', freq='S')  # for Lear
+    # rdm_idx = pd.date_range(start='2019-09-09 0:51:57', periods=10, tz='UTC', freq='S')  # for Lear
     # rdm_idx = pd.date_range(start='2019-09-06 23:58:30', periods=60, tz='UTC', freq='S')  # for P3B
     # rdm_idx = df_concat.index
     indexx = rdm_idx
@@ -288,33 +380,103 @@ def main():
     attrs_merged = get_attrs_merged(df_concat.attrs, _upper, _lower)
     ls = []
     ls_z = []
+    params = pd.DataFrame(index=indexx, columns=['lwc', 'dm', 'nw', 'z', 'r'])
     for i in indexx:
         df = df_concat.loc[i]
-        res1 = df.unstack().T  # .reindex(index=np.arange(0.5, 10000, 0.25))
+        res1 = df.unstack().T
         if res1['2DS10'].dropna(how='any').empty:
-            df1 = pd.Series(index=df_concat.attrs['2DS10']['bin_cent'], dtype='float64')
+            df1 = pd.Series(index=df_concat.attrs['2DS10']['bin_cent'], dtype='float64', name='2DS10')
+        elif len(res1['2DS10'].dropna(how='any')) < len(df_concat.attrs['2DS10']['bin_cent']):
+            df1 = pd.Series(index=df_concat.attrs['2DS10']['bin_cent'], dtype='float64', name='2DS10')
         else:
             df1 = res1['2DS10'].dropna(how='any')
 
         if res1['HVPS'].dropna(how='any').empty:
-            df2 = pd.Series(index=df_concat.attrs['HVPS']['bin_cent'], dtype='float64')
+            df2 = pd.Series(index=df_concat.attrs['HVPS']['bin_cent'], dtype='float64', name='HVPS')
+        elif len(res1['HVPS'].dropna(how='any')) < len(df_concat.attrs['HVPS']['bin_cent']):
+            df2 = pd.Series(index=df_concat.attrs['HVPS']['bin_cent'], dtype='float64', name='HVPS')
         else:
             df2 = res1['HVPS'].dropna(how='any')
-
-        a = linear_wgt(df1=df1, df2=df2, ovr_upp=_upper, ovr_lower=_lower)
+        df1.attrs = df_concat.attrs
+        df2.attrs = df_concat.attrs
+        a = linear_wgt(df1=df1, df2=df2, ovr_upp=_upper, ovr_lower=_lower, method='snal')
         a.attrs['dsizes'] = attrs_merged
         a.attrs['instrument'] = 'Composite_PSD'
-        z = ref_calc(a)
-        ls_z.append(z)
+        params.loc[i] = pds_parameters(a)
+        ls_z.append(ref_calc(a))
         ls.append(a)
 
     df_reflectivity = pd.concat(ls_z, axis=1).T.set_index(indexx)
     df_merged = pd.concat(ls, axis=1).T.set_index(indexx)
+    df_add = get_add_data(aircraft, indexx=indexx)
 
     df_merged.attrs['dsizes'] = attrs_merged
     df_day = df_merged.groupby(df_merged.index.floor('d'))
     keys = list(df_day.groups.keys())
     del df_day
+
+    # for key in keys:
+    #     airc = ls_df[0].attrs['aircraft']
+    #     df = df_merged.groupby(df_merged.index.floor('d')).get_group(key)
+    #     df = df[df > 0]
+    #     fig, ax = plt.subplots(figsize=(14, 6))
+    #     cbar = ax.pcolormesh(df.index, df.columns * 1e-3, np.log10(df.T * 1e6), vmin=0, vmax=10, cmap='jet')
+    #     plt.colorbar(cbar, ax=ax, pad=0.01, aspect=20)  # .set_ticks(np.arange(0,,1))
+    #     ax.set_ylabel(r'$Diameter \  (mm)$', fontsize='x-large')
+    #     ax.set_xlabel('$Time \  (UTC)$', fontsize='x-large')
+    #     ax.set_title('$N(D), \log_{10} (\# \ m^{-3} mm^{-1}) $', position=(0.8, 0.1), fontsize='x-large')
+    #     ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+    #     ax.set_yscale('log')
+    #     title = f"{key: %Y-%m-%d} UTC - {aircraft}"
+    #     fig.suptitle(title, fontsize=14, fontweight='bold', y=0.99)
+    #     plt.tight_layout()
+    #     # path_save = f'{path_data}/results/bimodality/flight/{aircraft}'
+    #     # make_dir(path_save)
+    #     # fig.savefig(f"{path_save}/{aircraft}_{key:%Y%m%d}.jpg")
+    #     plt.show()
+    #     print(1)
+
+    for key in keys:
+        airc = ls_df[0].attrs['aircraft']
+        df = df_merged.groupby(df_merged.index.floor('d')).get_group(key)
+        df_z = df_concat.groupby(df_concat.index.floor('d')).get_group(key)
+        # df_z = df_z.loc[(df_z.index > '2019-09-07 02:33:00') & (df_z.index < '2019-09-07 02:33:55')]
+        df_z = df_z.loc[(df_z.index > indexx.min()) & (df_z.index < indexx.max())]
+        fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(12, 10))
+        cbar = ax1.pcolormesh(df.index, df.columns * 1e-3, np.log10(df.T * 1e6), vmin=0, vmax=10, cmap=my_cmap)
+        cbar2 = ax2.pcolormesh(df_z['2DS10'].index, df_z['2DS10'].columns * 1e-3, np.log10(df_z['2DS10'].T * 1e6),
+                               vmin=0,
+                               vmax=10, cmap=my_cmap)
+        cbar3 = ax3.pcolormesh(df_z['HVPS'].index, df_z['HVPS'].columns * 1e-3, np.log10(df_z['HVPS'].T * 1e6), vmin=0,
+                               vmax=10, cmap=my_cmap)
+
+        plt.colorbar(cbar, ax=ax1, pad=0.01, aspect=20)  # .set_ticks(np.arange(0,,1))
+        plt.colorbar(cbar2, ax=ax2, pad=0.01, aspect=20)  # .set_ticks(np.arange(0,,1))
+        plt.colorbar(cbar3, ax=ax3, pad=0.01, aspect=20)  # .set_ticks(np.arange(0,,1))
+        ax1.set_ylabel(r'$Diameter \  (mm)$', fontsize='x-large')
+        ax1.set_xlabel('$Time \  (UTC)$', fontsize='x-large')
+        ax1.set_title('$N(D), \log_{10} (\# \ m^{-3} mm^{-1}) $', position=(0.8, 0.1), fontsize='x-large')
+        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+        ax1.set_yscale('log')
+        ax2.set_yscale('log')
+        ax3.set_yscale('log')
+        ax2.set_title('2DS10')
+        ax3.set_title('HVPS')
+        ax1.set_xlim(df_merged.index.min(), df_merged.index.max())
+        ax2.set_xlim(df_merged.index.min(), df_merged.index.max())
+        ax3.set_xlim(df_merged.index.min(), df_merged.index.max())
+        ax1.set_ylim(0.01, 40)
+        ax2.set_ylim(0.01, 40)
+        ax3.set_ylim(0.01, 40)
+
+        title = f"{key: %Y-%m-%d} UTC - {aircraft}"
+        fig.suptitle(title, fontsize=14, fontweight='bold', y=0.99)
+        plt.tight_layout()
+        # path_save = f'{path_data}/results/bimodality/flight/{aircraft}'
+        # make_dir(path_save)
+        # fig.savefig(f"{path_save}/{aircraft}_{key:%Y%m%d}.jpg")
+        plt.show()
+        print(1)
 
     for key in keys:
         airc = ls_df[0].attrs['aircraft']
