332ab18ed131f2eab901ad1cd7d265b250481361
Alfonso Ladino <aladinor@unal.edu.co>
2023-05-31 09:45:02 -0500

    fixing problem when estimating mu values using williams and campex equations

--- src/pds_retrievals.py
+++ src/pds_retrievals.py
@@ -74,17 +74,17 @@ def wrapper(dms, dm, ds):
 def root(dms_dfr, dm, dfr, z_ku):
     try:
         if (dfr < 0) & (z_ku < 10):
-            idxmax = np.argmax(dms_dfr)
+            idxmax = np.nanargmax(dms_dfr)
             dms = dms_dfr[:idxmax]
-            idmin = np.argmin(np.abs(dms - 0.0))
+            idmin = np.nanargmin(np.abs(dms - 0.0))
             return dm[idmin]
         elif (dfr < 0) & (z_ku > 20):
-            idxmax = np.argmax(dms_dfr)
+            idxmax = np.nanargmax(dms_dfr)
             dms = dms_dfr[idxmax:]
-            idmin = np.argmin(np.abs(dms - 0.0))
+            idmin = np.nanargmin(np.abs(dms - 0.0))
             return dm[idmin + idxmax]
         else:
-            idmin = np.argmin(np.abs(dms_dfr - 0.0))
+            idmin = np.nanargmin(np.abs(dms_dfr - 0.0))
             return dm[idmin]
     except ValueError:
         return np.nan
